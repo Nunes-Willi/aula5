0c446522c15f50ac2bbd8b6fa8065392cbe1db52
nunes-willi <nuneswillroberto@gmail.com>
2025-10-16 09:24:12 -0300

    first commit

---
+++ aula5.py
@@ -0,0 +1,18 @@
+import math, datetime, random, string
+
+#TODO 01
+# a = float(input("Digite um número real: "))
+# print(f"{math.sqrt(a):.2f}")
+
+#TODO 02
+print(datetime.datetime.now())
+
+#TODO 03
+import string
+
+at = input("Digite uma frase/palavra: ")
+if any(char in string.digits for char in at):
+    print("A frase/palavra contém números.")
+else:
+    print("A frase/palavra não contém números.")
+
